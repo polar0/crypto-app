0b81b3538677e26a70459bbcafdee51956d834a6
polar0 <23r0eth@gmail.com>
2022-03-26 15:59:17 +0100

    Initial template commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './style.css';
+
+// Etherscan
+// 5RTBT9H4WF8D61R19UFE78Z9SMBJ6S1J9V
+
+// Get Ether Last Price
+
+// https://api.etherscan.io/api
+//    ?module=stats
+//    &action=ethprice
+//    &apikey=YourApiKeyToken
+
+// Get estimation of confirmation time (with gas paid)
+
+// https://api.etherscan.io/api
+//    ?module=gastracker
+//    &action=gasestimate
+//    &gasprice=2000000000
+//    &apikey=YourApiKeyToken
+
+// Get Eth balance for a Single Adress
+// Par exemple avec des personnes connues
+
+// https://api.etherscan.io/api
+//    ?module=account
+//    &action=balance
+//    &address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+//    &tag=latest
+//    &apikey=YourApiKeyToken
+
+// ATTRIBUTION
+// Powered by Etherscan.io APIs
+// Powered by CoinGecko
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Crypto App',
+      template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/assets/images', to: 'assets/images' }],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|xml|ttf|woff|woff2|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
